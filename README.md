0d71ab28a33ab934e3f009d76a365b7c0ea18c6c
bevans14 <146108421+bevans14@users.noreply.github.com>
2023-10-28 18:16:41 -0400

    last commit

--- README.md
+++ README.md
@@ -5,8 +5,10 @@ To mute the backhround audio, simply click the "mute" button in the top left.
 To play, you must choose rock, paper, or scissors. You can do this by clicking
 on the images below the "Choose your weapon!" text.
 
-You can also utilize the input box. You must input rock, paper, or scissors, otherwise there will be an alert telling you to typee a valid response.
+You can also utilize the input. You must input rock, paper, or scissors, otherwise there will be an alert telling you to typee a valid response.
 
 Click the "submit" button to finalize your choice.
 
-Once you have submit your choice, the opponents choice will display, notice how the score goes up for the winner. 
+Once you have submit your choice, the opponents choice will display, notice how the score goes up for the winner.
+
+To reset the score, click the "play again" button.
--- Script.js
+++ Script.js
@@ -6,24 +6,22 @@ var opponent;
 var opponentScore = 0;
 var choices = ["rock", "paper", "scissors"];
 var previousScore = 0;
+var opponentPrevScore = 0; 
+winLose = ''; // empty string because the words displayed will be modified
 
-
-// player choice
-
-window.onload = function(){ // when page loads function starts
+window.onload = function(){ 
     for (let i = 0; i < 3; i++) {
-        let choice = document.createElement("img"); // creates element node and connects it to the DOM
+        let choice = document.createElement("img");
         choice.id = choices[i];
-        choice.src = choices[i] + ".png"; // photos match array name
+        choice.src = choices[i] + ".png"; 
         choice.addEventListener("click", selectChoice);
-        document.getElementById("choices").append(choice); // displays in box
+        document.getElementById("choices").append(choice);
     }
 }
 
+// This function determines the winner
 
-
-
-function selectChoice(userInp){
+function selectChoice(userInp){ // Userinp will only run when called
     
     you = this.id;
     document.getElementById("your-choice").src = you + ".png"
@@ -31,9 +29,9 @@ function selectChoice(userInp){
     opponent = choices[Math.floor(Math.random() * 3)];
     document.getElementById("opponent-choice").src = opponent + ".png";
 
-    if (userInp === opponent) {
-        yourScore += 1;
-        opponentScore += 1;
+    if (userInp === opponent) { // this works because of the paramater in line 24. "===" is absolute.
+        yourScore += 1; // Score was initially set to 0 and because it's not const, the value is increased by 1
+        opponentScore += 1; 
     }
     else {
         if (userInp === "rock") {
@@ -67,6 +65,7 @@ function selectChoice(userInp){
 if (you === opponent) {
     yourScore += 1;
     opponentScore += 1;
+
 }
 else {
     if (you === "rock") {
@@ -96,30 +95,37 @@ else {
         }
     }
 
-    
 }
 
-document.getElementById("your-score").innerText = yourScore;
-document.getElementById("opponent-score").innerText = opponentScore;
+if (yourScore === previousScore + 1 && opponentScore === opponentPrevScore + 1) {
+    winLose = "You Tied!";
+    winPoint.play();
+// if both scores go up, winPoint audio will play and "You Tied!" will display.
+}
 
-if (yourScore === previousScore + 1) {
+else if (opponentScore == opponentPrevScore) {
     const winPoint = document.getElementById("winPoint");
     winPoint.play();
+    winLose = "You Won!"
 }
-
+else if (opponentScore === opponentPrevScore + 1)
+    {
+        winLose = "You Lost!"
+    }
+   
+document.getElementById("your-score").innerText = yourScore;
+document.getElementById("opponent-score").innerText = opponentScore;
+document.getElementById("WL").innerText = winLose
 previousScore = yourScore;
+opponentPrevScore = opponentScore;
+
+// Connecting the DOM allows for JavaScript to know what to grab.
 
 }
-
-
-
-
-
 function submitUserChoice() {
     var userInp = document.getElementById("input").value.toLowerCase();
     if (choices.includes(userInp)) {
-    // Set opponent's choice here
-    selectChoice(userInp);
+    selectChoice(userInp); // if userinput is rock, paper, or scissors, selectChoice function will run
     
     } else {
         alert("Invalid choice. Please enter 'rock', 'paper', or 'scissors'.");
