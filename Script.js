74db3daacf3dc284dfa0f53075cb7e2053388539
bevans14 <146108421+bevans14@users.noreply.github.com>
2023-10-28 13:05:48 -0400

    commit

---
+++ Script.js
@@ -0,0 +1,127 @@
+
+
+var you;
+var yourScore = 0;
+var opponent;
+var opponentScore = 0;
+var choices = ["rock", "paper", "scissors"];
+
+
+// player choice
+
+window.onload = function(){ // when page loads function starts
+    for (let i = 0; i < 3; i++) {
+        let choice = document.createElement("img"); // creates element node and connects it to the DOM
+        choice.id = choices[i];
+        choice.src = choices[i] + ".png"; // photos match array name
+        choice.addEventListener("click", selectChoice);
+        document.getElementById("choices").append(choice); // displays in box
+    }
+}
+
+
+function selectChoice(userInp){
+    // choice listens to this event b/c of line 17
+
+
+    if (userInp === opponent) {
+        yourScore += 1;
+        opponentScore += 1;
+    }
+    else {
+        if (userInp === "rock") {
+            if (opponent === "scissors") {
+                yourScore += 1;
+            }
+            else if (opponent === "paper") {
+                opponentScore +=1;
+            }
+        }
+        else if (userInp === "scissors") {
+            if (opponent === "paper") {
+                yourScore += 1;
+            }
+            else if (opponent === "rock") {
+                opponentScore +=1;
+            }
+        }
+       else if (userInp === "paper") {
+            if (opponent === "rock") {
+                yourScore += 1;
+            }
+            else if (opponent === "scissors") {
+                opponentScore +=1;
+            }
+        }
+    
+
+    }
+    
+   
+    you = this.id;
+    document.getElementById("your-choice").src = you + ".png"
+
+    opponent = choices[Math.floor(Math.random() * 3)];
+    document.getElementById("opponent-choice").src = opponent + ".png";
+
+if (you === opponent) {
+    yourScore += 1;
+    opponentScore += 1;
+}
+else {
+    if (you === "rock") {
+        if (opponent === "scissors") {
+            yourScore += 1;
+        }
+        else if (opponent === "paper") {
+            opponentScore +=1;
+        }
+    }
+    else if (you === "scissors") {
+        if (opponent === "paper") {
+            yourScore += 1;
+        }
+        else if (opponent === "rock") {
+            opponentScore +=1;
+        }
+    }
+   else if (you === "paper") {
+        if (opponent === "rock") {
+            yourScore += 1;
+        }
+        else if (opponent === "scissors") {
+            opponentScore +=1;
+        }
+    }
+
+    
+}
+
+document.getElementById("your-score").innerText = yourScore;
+document.getElementById("opponent-score").innerText = opponentScore;
+
+}
+
+
+
+function submitUserChoice() {
+    var userInp = document.getElementById("input").value.toLowerCase();
+    if (choices.includes(userInp)) {
+    // Set opponent's choice here
+    
+    
+    selectChoice(userInp);
+    
+    
+    } else {
+        alert("Invalid choice. Please enter 'rock', 'paper', or 'scissors'.");
+
+    }
+    
+    document.getElementById("your-choice").src = userInp + ".png";
+    document.getElementById("opponent-choice").src = opponent + ".png";
+
+}
+
+
+
